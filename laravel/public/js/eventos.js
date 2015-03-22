0b012ee25ced14d7e09dddf7dc47756e90b17094
Juan Antonio Beato Quiñones <juan.beato44@gmail.com>
2015-03-22 17:42:03 +0100

    Efectos Slider Servicios y modificaciones en CSS

--- laravel/public/js/eventos.js
+++ laravel/public/js/eventos.js
@@ -109,21 +109,147 @@ function ocultar(){
 }
 
 
-// prueba elementos que aparecen
+// Marcar en menú la sección en la que te encuentras
 
-// $(function() {  
-//     var altoProductos = $("#productos").outerHeight() + 200;
+$(function() {  
+    var altoInicio = $("#home").outerHeight() - 30;
+    var altoServicios = ($("#slide").outerHeight() - 30) + altoInicio;
+    var altoNosotros = ($("#nosotros").outerHeight() - 30) + altoServicios;
+    var formulario = ($("#formularios").outerHeight() - 30) + altoNosotros;
+
+    $(window).scroll(function(event) {
+
+       height = $(event.target).scrollTop();
+       if (height < altoInicio) {
+          $('.mainMenu_item').css({"background":"#202020"});
+          $('#linkHome').css({"background":"#FFA533"});
+       }
+       else if (height > altoInicio && height < altoServicios) {
+          $('.mainMenu_item').css({"background":"#202020"});
+          $('#linkSlide').css({"background":"#FFA533"});
+       }
+       else if (height > altoServicios && height < altoNosotros) {
+          $('.mainMenu_item').css({"background":"#202020"});
+          $('#linkNosotros').css({"background":"#FFA533"});
+       }
+       else if (height > altoNosotros) {
+          $('.mainMenu_item').css({"background":"#202020"});
+          $('#linkFormulario').css({"background":"#FFA533"});
+       }
+
+    });
+});
 
 
-//     var altoNosotros = $("#nosotros").outerHeight() + altoProductos;
-//     var formulario = $("#formularios").outerHeight() + altoNosotros;
+// Animaciones
 
-//     $(window).scroll(function(event) {
+function ejemplo(){
+  $('.comp1 .slide_title').delay(200).animate({"left":"0%","opacity":"0"},800,'easeInOutBack');
+  $('.comp1 .slide_description').delay(400).animate({"left":"0%","opacity":"0"},800,'easeInOutBack');
+  $('.comp1 .slide_imageContainer').delay(600).animate({"right":"80%","opacity":"0"},1000,'easeInOutBack');
+}
 
-//         height = $(event.target).scrollTop();
 
-//        if (height > altoProductos && height < altoNosotros) {
-//             $('.profile').fadeIn(4000);
-//        }
-//     });
-// });
+function borrar(objeto){
+  $(objeto).children().each(function(){
+    $(this).css({"opacity":"0"});
+  });
+}
+
+function muevelosIzq(){
+  mueveComp1Izquierda();
+  mueveComp2Izquierda();
+}
+
+function muevelosDer(){
+  mueveComp1Derecha();
+  mueveComp2Derecha();
+}
+
+function mueveComp1Izquierda(){
+  $('#comp1').children().each(function(){
+    if($(this).hasClass("slide_title")){
+      $(this).delay(200).animate({"left":"0%","opacity":"0"},800,'easeInOutBack');
+    }
+    else if($(this).hasClass("slide_description")){
+      $(this).delay(400).animate({"left":"0%","opacity":"0"},800,'easeInOutBack');
+    }
+    else if($(this).hasClass("logoDW") || $(this).hasClass("logoDWB")){
+      $(this).delay(100).animate({"opacity":"0"},600,'easeInOutBack');
+    }
+    else{
+      $(this).delay(600).animate({"right":"80%","opacity":"0"},1000,'easeInOutBack');
+    }
+  });
+  $('#comp1').css({"z-index":"1"});
+}
+
+function mueveComp1Derecha(){
+  $('#comp1').children().each(function(){
+    if($(this).hasClass("slide_title")){
+      $(this).delay(400).animate({"left":"17%","opacity":"1"},2000,'easeInOutBack');
+    }
+    else if($(this).hasClass("slide_description")){
+      $(this).delay(600).animate({"left":"17%","opacity":"1"},1800,'easeInOutBack');
+    }
+    else if($(this).hasClass("logoDW") || $(this).hasClass("logoDWB")){
+      $(this).delay(900).animate({"opacity":"1"},2000,'easeInOutBack');
+    }
+    else{
+      $(this).delay(800).animate({"right":"13%","opacity":"1"},1800,'easeInOutBack');
+    }
+  });
+  $('#comp1').css({"z-index":"1"});
+}
+
+function mueveComp2Izquierda(){
+  $('#comp2').css({"opacity":"1"});
+  $('#comp2').children().each(function(){
+    if($(this).hasClass("slide_title_right")){
+      $(this).delay(400).animate({"left":"20%","opacity":"1"},1600,'easeInOutBack');
+    }
+    else if($(this).hasClass("slide_description_right")){
+      $(this).delay(600).animate({"left":"20%","opacity":"1"},1600,'easeInOutBack');
+    }
+    else{
+      $(this).delay(800).animate({"right":"5%","opacity":"1"},1800,'easeInOutBack');
+    }
+  });
+  $('#comp2').css({"z-index":"2"});
+}
+
+function mueveComp2Derecha(){
+  $('#comp2').css({"opacity":"1"});
+  $('#comp2').children().each(function(){
+    if($(this).hasClass("slide_title_right")){
+      $(this).delay(600).animate({"left":"70%","opacity":"0"},1600,'easeInOutBack');
+    }
+    else if($(this).hasClass("slide_description_right")){
+      $(this).delay(400).animate({"left":"70%","opacity":"0"},1600,'easeInOutBack');
+    }
+    else{
+      $(this).delay(200).animate({"right":"0%","opacity":"0"},1400,'easeInOutBack');
+    }
+  });
+  $('#comp2').css({"z-index":"2"});
+}
+
+// setInterval('movimiento()',9000);
+
+var primero = false;
+var segundo = false;
+
+function movimiento(){
+  if(primero == false){
+    primero = true;
+    segundo = false;
+    muevelosIzq();
+  }
+  else{
+    segundo = true;
+    primero = false;
+    muevelosDer();
+  }
+}
+
+
